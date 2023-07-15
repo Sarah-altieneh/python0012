8b661bb6b7ebab4f1edd99704abbf4e02e0d613d
Sarah-altieneh <2020980050@ses.yu.edu.jo>
2023-07-16 00:42:19 +0300

    python

--- app.py
+++ app.py
@@ -7,3 +7,29 @@ print(type(compNum))
 print(compNum)
 str1="name"
 print(str1)
+#quiz :
+person1={'name':'sarah','age':21,'address':'Irbid','phone number':'0789642365','email':'sarah@gmail.com'}
+print (person1['name'])
+print(person1['email'])
+person2={'name':'seedra','age':16,'address':'Irbid','phone number':'078964288','email':'seedra@gmail.com'}
+print (person2['name'])
+print(person2['email'])
+person3={'name':'marah','age':28,'address':'Irbid','phone number':'0789642365','email':'marah@gmail.com'}
+print (person3['name'])
+print(person3['email'])
+person4={'name':'morshed','age':25,'address':'Irbid','phone number':'0789642365','email':'morshed@gmail.com'}
+print (person4['name'])
+print(person4['email'])
+person5={'name':'mazin','age':60,'address':'Irbid','phone number':'0789642365','email':'mazin@gmail.com'}
+print (person5['name'])
+print(person5['email'])
+person1['age']=22
+person2['age']=18
+person3['age']=29
+person4['age']=26
+person5['age']=61
+print(person1['age'])
+print(person2['age'])
+print(person3['age'])
+print(person4['age'])
+print(person5['age'])
