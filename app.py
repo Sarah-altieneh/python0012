d6532809d270057cacdbbad0cb9f6a1d882000bd
Sarah-altieneh <2020980050@ses.yu.edu.jo>
2023-08-07 22:21:20 +0300

    up

--- app.py
+++ app.py
@@ -190,34 +190,106 @@ person['parents']={
  #   print(classRoom[i])
   #  print('----------------------------------')
 
-student ={}
-inserstTime=int(input('how many keys do you want to add: '))
-for i in range (inserstTime):
-  stkey=input('Enter the key: ')
-  keytype=int(input('enter the type of the key 1 for str 2 for int 3 for list 4 for dic: '))
-  if (int(keytype)==1):
-     student[stkey]=input('enter the value of '+stkey+': ')
-  elif(int(keytype)==2):   
-            tryAndEX=True
-            while tryAndEX:
-              try:
-                  student[i]=int(input('enter your age : '))
-                  tryAndEX=False
-              except ValueError:
-                print('not number ') 
-  elif(int(keytype)==3):
+#student ={}
+#inserstTime=int(input('how many keys do you want to add: '))
+#for i in range (inserstTime):
+ # stkey=input('Enter the key: ')
+ # keytype=int(input('enter the type of the key 1 for str 2 for int 3 for list 4 for dic: '))
+ # if (int(keytype)==1):
+  #   student[stkey]=input('enter the value of '+stkey+': ')
+  #elif(int(keytype)==2):   
+   #         tryAndEX=True
+    #        while tryAndEX:
+     #         try:
+      #            student[i]=int(input('enter your age : '))
+       #           tryAndEX=False
+        #      except ValueError:
+         #       print('not number ') 
+  #elif(int(keytype)==3):
      
-     skillsElements = int(input("Enter the number of "+stkey+"skills you have : "))
-     student[stkey]=[]
-     for i in range(skillsElements):
-        student[stkey].append(input('enter the'+stkey+': '))
-  elif(int(keytype)==4):                     
-    time=int(input('enter number of dectionary you want to add: '))
-    for i in range(time):             
-      newDec =input('enter new key :')
-      student[newDec]={}
-      time1=int(input('enter number of '+ newDec +' you want to add: '))
-      for j in range (time1):
-        newSubKey=input ('enter '+newDec+'number '+str(j+1)+': ')
-        student[newDec][newSubKey]=input('enter the value : ')
-print (student)
+   #  skillsElements = int(input("Enter the number of "+stkey+"skills you have : "))
+    # student[stkey]=[]
+    # for i in range(skillsElements):
+     #   student[stkey].append(input('enter the'+stkey+': '))
+  #elif(int(keytype)==4):                     
+   # time=int(input('enter number of dectionary you want to add: '))
+    #for i in range(time):             
+     # newDec =input('enter new key :')
+      #student[newDec]={}
+     # time1=int(input('enter number of '+ newDec +' you want to add: '))
+      #for j in range (time1):
+       # newSubKey=input ('enter '+newDec+'number '+str(j+1)+': ')
+        #student[newDec][newSubKey]=input('enter the value : ')
+#print (student)
+
+class BankAccount:
+    acc_name=''
+    acc_num=0
+    balance=0
+    def __init__(self,acc_name,acc_num,balance,password):
+        self.acc_name=acc_name
+        self.acc_num=acc_num
+        self.balance=balance
+        self.password=password
+    def __str__(self):
+        return f'name is {self.acc_name} and the account number is {self.acc_num} and the belence is {self.balance}'
+    def withdraw(self, amount):
+       print ('you are about to withdraw.')
+       passw=input('enter your password to confirme you withdraw operation : ')
+     
+       try:
+            if(self.password==passw):
+              if (self.balance)<int(amount):
+                  return 'you do not have engough money'
+              self.balance-=int(amount)
+              return (f'your balance is {self.balance} and you withdraw {amount}')
+            else :
+                return('you enter wrong password')
+       except ValueError:
+              return 'you entered wrong value '
+    def deposit (self, amount):
+        print('you are about to deposite.')
+        passw=input('enter your password to confirme your deposite operation : ')
+        try:
+           if(self.password==passw):
+              self.balance+=int(amount)
+              return (f'your balance is {self.balance} and you deposit {amount}')
+           else :
+                return('you enter wrong password')
+        except ValueError:
+            return 'you entered wrong value '
+    def checkAccount(self):
+        return f'your balance {self.balance}'
+    def closeAccount (self):
+        print('you are about to close your account.')
+        passw=input('enter your password to confirme your closeing operation : ')
+        print(self.withdraw(self.balance))
+        self.acc_name='The account is closed'
+        self.acc_num=0
+        self.balance=0
+        return f'name is {self.acc_name} and the account number is {self.acc_num} and the belence is {self.balance}'
+        
+userName=input('enter your name :') 
+accNum=input('enter the account number : ')
+accbalance=int(input('enter the balance in your account :')) 
+passwor=input('enter you password: ')      
+account1=BankAccount(userName,accNum,accbalance,passwor)
+check=True
+while check:
+  newop=input('Do you want to make new operatio (yes or no):')  
+  if newop=='yes':
+    opkey=int(input('enter the type of the key 1 for withdraw 2 for deposite 3 for check balance 4 for close account: '))
+    if (int(opkey)==1):
+        amount=(input('enter the amount you want to withdraw: '))
+        print(account1.withdraw(amount))     
+    elif  (int(opkey)==2):  
+          amount=(input('enter the amount you want to withdraw: '))
+          print (account1.deposit(amount))
+    elif(int(opkey)==3):      
+      print(account1.checkAccount())
+    elif(int(opkey)==4):  
+      print (account1.closeAccount())
+  else:
+      check=False
+      print('Thank you !')    
+ 
